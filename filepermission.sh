5cfbbb25b8c31a21f4ff24fcea8cccc5a3d63b0c
Your Name <your.email@example.com>
2023-11-29 17:55:30 -0800

    FilePermission

---
+++ filepermission.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script Name:                  challenge01
+# Author:                       scotty Jokon
+# Date of latest revision:      11/29/2023
+# Purpose:                      Change permission for files in directory
+# Source                        https://chat.openai.com/share/0955b49a-841e-42b6-b77f-568c3c18afdd
+
+
+
+# Function to change permission
+Changne_permissions() {
+    directory="$1"
+    permissions"$9"
+}
+
+# Prompt user for input directory path
+read -p "Enter the directory path: " directory
+
+# Prompt user for input permissions number
+read -p "Enter the permissions number (e.g., 777): " permissions
+
+# Change permissions of all files in the directory
+chmod -R "$permissions" *
