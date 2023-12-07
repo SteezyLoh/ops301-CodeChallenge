cea5ece9f324add5e72eb073fb74b4f979c16759
Your Name <your.email@example.com>
2023-12-06 22:52:10 -0800

    update readme

--- Class08.py
+++ Class08.py
@@ -1,10 +1,10 @@
-Python3
+
 
 # Objective
 # Create a Python script that includes the following:
 # Assign to a variable a list of ten string elements.
 # Step 1
-my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+my_list = ["apple", "banana", "cherry", "carrots", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
 
 # Print the fourth element of the list.
 # Step 2
@@ -17,6 +17,8 @@ print("Sixth to Tenth elements:", my_list[5:])
 
 # Change the value of the seventh element to “onion”.
 # Step 4
-my_list[6] = "onion"
+my_list[6] = "grapes"
 
 print('updated list:', my_list)
+
+# Resource : https://chat.openai.com/share/e738cc10-3a14-4f60-b39f-3a60a534bd43
