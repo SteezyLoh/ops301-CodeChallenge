b81eb6e3b43b54f7b6bb9126985f3944c7c32dab
Your Name <your.email@example.com>
2023-12-07 20:57:42 -0800

    Py conditional statements
    git push
    cd.
    scottyj#Labkit692:~/ops301-CodeChallenge$ cd ..
    
    Use AI-based tools to accelerate learning and problem-solving
    Apply lightweight Agile project management methodologies
    Administer critical IT and network systems
    Practice identity management in Active Directory (AD)/LDAP protocol
    Design and deploy a client-server network including a domain controller (DC)
    Create and push group policy to endpoints
    Analyze how cloud systems can integrate into on-premise systems
    Automate OS tasks using scripting languages Bash, Powershell, and Python
    Design and deploy network infrastructure using the open source pfSense virtual appliance
    Extend the capabilities of pfSense using packages
    Troubleshoot challenging network connectivity problems
    Oversee patch management of Windows endpoints

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ November/29/2023
 - [Day 05 - ClearingLog](ClearingLogs.sh)
 - [Day 06 - Bash in Python](Class06.py)
 - [Day 07 - Directory Creation](class07.py)
+- [Day 08 - Python CollectionsOverview](Class08.py)
---
+++ class09.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Arthur:    Scotty Jokon
+# Date:      12/7/2023
+# Purpose:  Python Conditional Statements
+
+# T-shirt Prices
+Walmart = 10
+Adidas = 40
+Nike = 40
+Gucci = 1000
+Luis_Vuitton = 1500
+
+if Walmart == Gucci:
+    print('Yes, Ineed!')
+    # Is Adidas not equal
+elif Adidas != Nike:
+    print('Yes, These guy are the same')
+    # Is LV greater then Gucci
+elif Luis_Vuitton > Gucci:
+    print('Duh,SToopiD')
+else:
+    print('Theres levels to this!')  
+
+# End
+  
