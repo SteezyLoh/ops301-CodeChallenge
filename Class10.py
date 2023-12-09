ddb7cd06c12403609d3580fcc3f7949086d31138
Your Name <your.email@example.com>
2023-12-08 16:50:55 -0800

    Python File Handling Overview

---
+++ Class10.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Arthur:    Scotty Jokon
+# Date:      12/8/2023
+# Purpose:  Python File Handling Overview
+
+import os
+file = open("test.txt",  "w")
+
+file.write("This is the first line")
+11
+
+file.close()
+file = open("test.txt.","w")
+file.write("This is the second ")
+11
+
+file.close()
+file = open("text.txt", "a")
+file.write("\nThis is the last 2")
+13
+
+file.close()
+file = open("text.txt", "a")
+file.write("\nThis is the last 1")
+14
+
+
+
